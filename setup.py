ec1f2d4779625229398547b9234d0e9e874a2c9a
Ming Chen <ming.c@live.com>
2015-11-08 10:24:16 -0800

    fix syntax

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     install_requires=[
         'six',
     ],
-    description='Python CAS client library'
+    description='Python CAS client library',
     keywords=['cas', 'cas2', 'cas3', 'client', 'sso', 'single sign-on', 'authentication', 'auth'],
     license='MIT',
     long_description=readme,
