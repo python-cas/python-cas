dc09fa67e3ad35ce3fd78fc00abb6b63f1fead89
Nikolas Nyby <nnyby@columbia.edu>
2025-10-08 11:26:42 -0400

    :bookmark: release version 1.7.0

--- setup.py
+++ setup.py
@@ -40,5 +40,5 @@ setup(
     py_modules=['cas'],
     url='https://github.com/python-cas/python-cas',
     download_url ='https://github.com/python-cas/python-cas/releases',
-    version='1.6.0',
+    version='1.7.0',
 )
